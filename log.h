352103b1725556336f75d49a7d93e820c4cfe739
Alexander Qi <xdqi@users.noreply.github.com>
2017-10-24 09:28:23 +0800

    Fix compiler warnings

--- log.h
+++ log.h
@@ -17,9 +17,9 @@
 
 inline void hexdump(const void *input, size_t length) {
   const uint8_t *buf = reinterpret_cast<const uint8_t *>(input);
-  for (int i = 0; i < length; i += 16) {
-    fprintf(stderr, "%04X  ", i);
-    for (int j = 0; j < 16; j++) {
+  for (size_t i = 0; i < length; i += 16) {
+    fprintf(stderr, "%04zX  ", i);
+    for (size_t j = 0; j < 16; j++) {
       if (i + j < length) {
         fprintf(stderr, "%02x ", buf[i + j]);
       } else {
@@ -31,7 +31,7 @@ inline void hexdump(const void *input, size_t length) {
     }
     fprintf(stderr, "  ");
 
-    for (int j = 0; j < 16; j++) {
+    for (size_t j = 0; j < 16; j++) {
       if (i + j < length) {
         fprintf(stderr, "%c", isprint(buf[i + j]) ? buf[i + j] : '.');
       }
