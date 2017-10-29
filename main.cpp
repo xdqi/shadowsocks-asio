ea46e8718681f3ab4573c1bd269564ff95c7ebf9
Alexander Qi <xdqi@users.noreply.github.com>
2017-10-29 16:55:35 +0800

    Initialize cipher at start

--- main.cpp
+++ main.cpp
@@ -22,12 +22,12 @@ class TcpServer {
 public:
   TcpServer(boost::asio::io_service& io_service, uint16_t listen_port,
             const tcp::resolver::iterator &server_addresses,
-            const std::string &cipher, const std::string &password)
+            const Shadowsocks::AeadCipher *cipher, const std::string &password)
     : io_service_(io_service),
       acceptor_(io_service, tcp::endpoint(boost::asio::ip::address_v6::any(), listen_port)),
       socket_(io_service),
       server_addresses_(server_addresses),
-      cipher_(Shadowsocks::AeadCipher::get_cipher(cipher)),
+      cipher_(cipher),
       psk_(password_to_key((const uint8_t *) password.c_str(), password.length(), cipher_->key_size_)) {
       do_accept();
   }
@@ -76,7 +76,13 @@ int main(int argc, char* argv[]) {
       exit(-1);
     }
 
-    TcpServer s(io_service, std::atoi(argv[1]), server_addresses, argv[4], argv[5]);
+    auto cipher = Shadowsocks::AeadCipher::get_cipher(argv[4]);
+    if (!cipher) {
+      LOGE("Invalid cipher name %s", argv[4]);
+      exit(-1);
+    }
+
+    TcpServer s(io_service, std::atoi(argv[1]), server_addresses, cipher, argv[5]);
 
     LOGI("Listening on port %d", std::atoi(argv[1]));
     io_service.run();
